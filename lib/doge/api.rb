4135591118e1bdddab2c525a993a70e771bdcf82
Mariusz <mpruszynski@gmail.com>
2014-02-02 00:21:22 +0800

    proper parsing of the api outputs, also: the base uri should probably be a constant

--- lib/doge/api.rb
+++ lib/doge/api.rb
@@ -5,9 +5,19 @@ module DogeApi
   class DogeApi
     attr_accessor :api_key
 
+    BASE_URI = URI.parse 'https://www.dogeapi.com/wow'
+
+    CALLS_RETURNING_INTEGER = [:get_current_block]
+    CALLS_RETURNING_FLOAT   = [
+                                :get_balance          ,
+                                :get_address_received ,
+                                :get_difficulty       ,
+                                :get_current_price
+                              ]
+    CALLS_RETURNING_JSON    = [:get_my_addresses]
+
     def initialize api_key
-      @api_key  = api_key
-      @base_uri = URI.parse 'https://www.dogeapi.com/wow'
+      @api_key = api_key
     end
 
     def build_uri m, args
@@ -22,7 +32,18 @@ module DogeApi
     end
 
     def method_missing m, args = {}, &block
-      fetch_uri build_uri(m, args)
+      output = fetch_uri build_uri(m, args)
+      if CALLS_RETURNING_JSON.include? m
+        output = JSON.parse output
+      else
+        # Doge API wraps everything in double quotes,
+        # so we need to get rid of them first.
+        #
+        output.tr! '"', ''
+        output = output.to_i if CALLS_RETURNING_INTEGER.include? m
+        output = output.to_f if CALLS_RETURNING_FLOAT.include? m
+      end
+      output
     end
   end
 end
--- lib/doge/chain.rb
+++ lib/doge/chain.rb
@@ -1,15 +1,25 @@
 require 'uri'
 require 'open-uri'
+require 'json'
 
 module DogeApi
   class DogeChain
-    def initialize
-      @base_uri = 'http://dogechain.info/chain/Dogecoin/q'
-    end
+
+    BASE_URI = 'http://dogechain.info/chain/Dogecoin/q'
+
+    CALLS_RETURNING_INTEGER = [:getblockcount]
+    CALLS_RETURNING_FLOAT   = [
+                                :addressbalance       ,
+                                :getdifficulty        ,
+                                :getreceivedbyaddress ,
+                                :getsentbyaddress     ,
+                                :totalbc
+                              ]
+    CALLS_RETURNING_JSON    = [:transactions]
 
     def build_uri m, params
       list = params.join '/'
-      URI.parse "#{@base_uri}/#{m}/#{list}"
+      URI.parse "#{BASE_URI}/#{m}/#{list}"
     end
 
     def fetch_uri uri
@@ -17,7 +27,11 @@ module DogeApi
     end
 
     def method_missing m, *args, &block
-      fetch_uri build_uri(m, args)
+      output = fetch_uri build_uri(m, args)
+      output = output.to_i        if CALLS_RETURNING_INTEGER.include? m
+      output = output.to_f        if CALLS_RETURNING_FLOAT.include? m
+      output = JSON.parse(output) if CALLS_RETURNING_JSON.include? m
+      output
     end
   end
 end
