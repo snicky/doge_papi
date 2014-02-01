dcc4af7c8f5dabb4594871a1fae419353936985c
Mariusz <mpruszynski@gmail.com>
2014-02-01 23:28:54 +0800

    get rid of some unnecessary syntax

--- lib/doge/api.rb
+++ lib/doge/api.rb
@@ -5,23 +5,23 @@ module DogeApi
   class DogeApi
     attr_accessor :api_key
 
-    def initialize(api_key)
-      @api_key = api_key
-      @base_uri = URI.parse('https://www.dogeapi.com/wow')
+    def initialize api_key
+      @api_key  = api_key
+      @base_uri = URI.parse 'https://www.dogeapi.com/wow'
     end
 
-    def build_uri(m, args)
-      uri = @base_uri
-      params = args.merge(:a => m, :api_key => @api_key)
-      uri.query = URI.encode_www_form(params)
+    def build_uri m, args
+      uri       = @base_uri
+      params    = args.merge a: m, api_key: @api_key
+      uri.query = URI.encode_www_form params
       uri
     end
 
-    def fetch_uri(uri)
+    def fetch_uri uri
       uri.open.read
     end
 
-    def method_missing(m, args = {}, &block)
+    def method_missing m, args = {}, &block
       fetch_uri build_uri(m, args)
     end
   end
--- lib/doge/chain.rb
+++ lib/doge/chain.rb
@@ -7,17 +7,16 @@ module DogeApi
       @base_uri = 'http://dogechain.info/chain/Dogecoin/q'
     end
 
-    def build_uri(m, params)
+    def build_uri m, params
       list = params.join '/'
-      uri = URI.parse("#{@base_uri}/#{m}/#{list}")
-      uri
+      URI.parse "#{@base_uri}/#{m}/#{list}"
     end
 
-    def fetch_uri(uri)
+    def fetch_uri uri
       uri.open.read
     end
 
-    def method_missing(m, *args, &block)
+    def method_missing m, *args, &block
       fetch_uri build_uri(m, args)
     end
   end
